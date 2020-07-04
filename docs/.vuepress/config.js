8c76d0464fd488cdfcc90bb050dcbd727de1e7b7
qqlcx5 <893917884@qq.com>
2020-07-04 11:38:56 +0800

    blogCode

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,12 +9,10 @@ module.exports = {
     '@vssue/vuepress-plugin-vssue': {
       // set `platform` rather than `api`
       platform: 'github',
-
-      // all other options of Vssue are allowed
-      owner: 'lcx',
-      repo: 'https://qqlcx5.github.io/',
-      clientId: 'e97dc2555ad5694e1d7f',
-      clientSecret: 'fac6e9008f71c691b098f7d83c8973b946796213',
+      owner: 'qqlcx5',
+      repo: 'qqlcx5.github.io',
+      clientId: '012e6ab702f2bc7fe841',
+      clientSecret: 'c32fa7df3cda558d7b659bcbd7162f916844d1fd',
     },
   },
 };
