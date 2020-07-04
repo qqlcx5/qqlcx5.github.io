c629b836feda0adcc068c4cd8e4ffb16cec6b03d
qqlcx5 <893917884@qq.com>
2020-07-04 12:11:52 +0800

    blogCode

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,16 @@ git push git@gitee.com:qqlcx5/vuePressCode.git master
 
 npm run docs:build && cd docs/.vuepress/dist
 
-# 码云
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@gitee.com:qqlcx5/vuepress.git master
-
 # github
-rm -rf .git
 git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:qqlcx5/qqlcx5.github.io.git master
+
+# 码云
+rm -rf .git
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@gitee.com:qqlcx5/qqlcx5.git master
 cd -
