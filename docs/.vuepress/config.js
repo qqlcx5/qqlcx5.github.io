1b9ecfe146e7533b3a9a2839f018e5a9fc2de5ec
qqlcx5 <893917884@qq.com>
2020-07-05 21:55:49 +0800

    test2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,13 @@ const plugins = require('./plugins');
 const themeConfig = require('./themeConfig');
 
 module.exports = {
+  
   title: '廖纯鑫博客',
   description: '前端、前端教程、小程序、个人博客、廖纯鑫博客、vue、js、react、node、mysql',
   head: [
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
+  base: '/vuepress/',
   themeConfig,
   plugins
 };
