8fb1cb527ecb693190aa8c7d5e9a6360c4c025cc
qqlcx5 <893917884@qq.com>
2020-07-10 18:55:34 +0800

    s

--- docs/.vuepress/plugins.js
+++ docs/.vuepress/plugins.js
@@ -5,7 +5,7 @@ const plugins = {
   },
   '@vuepress-reco/vuepress-plugin-back-to-top': {
     customStyle: {
-      right:'3rem',
+      right: '3rem',
       bottom: '6rem',
       width: '2.5rem',
       height: '2.5rem',
@@ -17,14 +17,14 @@ const plugins = {
   },
   '@vuepress-reco/vuepress-plugin-bgm-player': {
     audios: [
-      { name: 'カワキヲアメク', artist: '美波', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/%E3%82%AB%E3%83%AF%E3%82%AD%E3%83%B2%E3%82%A2%E3%83%A1%E3%82%AF.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png'}
+      { name: 'カワキヲアメク', artist: '美波', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/%E3%82%AB%E3%83%AF%E3%82%AD%E3%83%B2%E3%82%A2%E3%83%A1%E3%82%AF.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png' },
       { name: 'Stories', artist: '黒石ひとみ', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/Stories.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/Stories.jpg' },
-      { name: '轻音乐', artist: '轻音乐', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png'}
+      { name: '轻音乐', artist: '轻音乐', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png' },
     ],
     autoShrink: true, //默认缩小
     shrinkMode: 'mini' //迷你模式
   },
-  '@vuepress/google-analytics':{'ga': 'UA-171545170-1' },
+  '@vuepress/google-analytics': { 'ga': 'UA-171545170-1' },
   '@vssue/vuepress-plugin-vssue': {
     platform: 'github-v4',
     owner: 'qqlcx5',
