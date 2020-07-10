54e96ba19dcb07bd9ca723ccb254d3524a248587
qqlcx5 <893917884@qq.com>
2020-07-10 18:41:56 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # 确保脚本抛出遇到的错误
 set -e
-git add -A
-git commit -m 'blogCode'
-git push git@gitee.com:qqlcx5/vuePressCode.git master
+# git add -A
+# git commit -m 'blogCode'
+# git push git@gitee.com:qqlcx5/vuePressCode.git master
 
 npm run docs:build && cd docs/.vuepress/dist
 
@@ -13,9 +13,9 @@ git commit -m 'deploy'
 git push -f git@github.com:qqlcx5/qqlcx5.github.io.git master
 
 # 码云
-rm -rf .git
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@gitee.com:qqlcx5/qqlcx5.git master
+# rm -rf .git
+# git init
+# git add -A
+# git commit -m 'deploy'
+# git push -f git@gitee.com:qqlcx5/qqlcx5.git master
 cd -
