2ca682a9177ae067b508a236ab2eba051658215c
qqlcx5 <893917884@qq.com>
2020-07-10 18:46:02 +0800

    s

--- docs/.vuepress/plugins.js
+++ docs/.vuepress/plugins.js
@@ -17,7 +17,8 @@ const plugins = {
   },
   '@vuepress-reco/vuepress-plugin-bgm-player': {
     audios: [
-
+      {name: '轻音乐', artist: '轻音乐', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png'},
+      { name: 'カワキヲアメク', artist: '美波', url: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/%E3%82%AB%E3%83%AF%E3%82%AD%E3%83%B2%E3%82%A2%E3%83%A1%E3%82%AF.mp3', cover: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png'}
     ],
     autoShrink: true, //默认缩小
     shrinkMode: 'mini' //迷你模式
