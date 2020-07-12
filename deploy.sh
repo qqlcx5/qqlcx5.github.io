5d24b10b8d498170e76aa06c0c8bc774f241901a
qqlcx5 <893917884@qq.com>
2020-07-12 23:55:38 +0800

    更新发布脚本

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,19 @@
 # 确保脚本抛出遇到的错误
 set -e
-# git add -A
-# git commit -m'blogCode'
-# git push git@gitee.com:qqlcx5/vuePressCode.git master
-
 npm run docs:build && cd docs/.vuepress/dist
-
-# github
+# github && 码云
 git init
 git add -A
 git commit -m 'deploy'
 git remote add origin git@github.com:qqlcx5/qqlcx5.github.io.git
 git remote set-url --add origin git@gitee.com:qqlcx5/qqlcx5.git
-git push -f master
+git push -f --all
+cd -
 
-# 码云
+# 码云(土方法 一个个仓库慢慢传)
 # rm -rf .git
 # git init
 # git add -A
 # git commit -m 'deploy'
 # git push -f git@gitee.com:qqlcx5/qqlcx5.git master
-cd -
+
