cc818bb2ffaa00ed6996bc7614a40453c197cf75
qqlcx5 <893917884@qq.com>
2020-07-12 22:26:21 +0800

    blogCode

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 git add -A
-git commit -m 'blogCode'
+git commit -m'blogCode'
 git push git@gitee.com:qqlcx5/vuePressCode.git master
 
 npm run docs:build && cd docs/.vuepress/dist
