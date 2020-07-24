43d33e82b535e4cb763b89a963043b1d9abbd0a3
qqlcx5 <893917884@qq.com>
2020-07-25 00:09:51 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 # 确保脚本抛出遇到的错误
 set -e
-npm run docs:build && cd docs/.vuepress/dist
+# npm run docs:build && cd docs/.vuepress/dist
 # github && 码云
 git init
 git add -A
 git commit -m 'deploy'
 git remote add origin git@github.com:qqlcx5/qqlcx5.github.io.git
 git remote set-url --add origin git@gitee.com:qqlcx5/qqlcx5.git
+git remote set-url --add origin git@e.coding.net:qqlcx5/blog/blog.git
 git push -f --all
 cd -
 
