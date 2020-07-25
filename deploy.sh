31cebc4df290b0f7d45e3523c7d878931c41ea07
qqlcx5 <893917884@qq.com>
2020-07-25 13:15:42 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set -e
 git init
 git add -A
 git commit -m 'deploy'
-git remote add origin git@github.com:qqlcx5/qqlcx5.github.io.git
-git remote set-url --add origin git@gitee.com:qqlcx5/qqlcx5.git
+# git remote add origin git@github.com:qqlcx5/qqlcx5.github.io.git
+git remote add origin git@gitee.com:qqlcx5/qqlcx5.git
 git remote set-url --add origin git@e.coding.net:qqlcx5/blog/blog.git
 git push -f --all
 cd -
