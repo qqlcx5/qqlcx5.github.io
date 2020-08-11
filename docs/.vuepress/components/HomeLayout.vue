da0144124f7b22c36ce83c14addf4e5912162f2e
another <893917884@qq.com>
2020-08-11 14:36:05 +0800

    update

--- docs/.vuepress/components/HomeLayout.vue
+++ docs/.vuepress/components/HomeLayout.vue
@@ -24,14 +24,15 @@
   </div>
 </template>
 <script>
-export default {
-  name: 'HomeLayout',
-  data () {
-    return {}
-  },
-  methods: {}
-}
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4307620a9933a7900b655b578321c816";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 </script>
+
 <style scoped>
 .HomeLayout {
   /* height: 100vh;
