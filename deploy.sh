bf3523aeb186df30d2c96603e284633ce8c47f2e
qqlcx5 <qqlcx5@163.com>
2021-02-20 17:30:52 +0800

    git@gitee.com:qqlcx5/qqlcx5.git

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git add -A
 date=`date +%Y-%m-%d_%H:%M:%S`
 git commit -m "deploy ${date}"
 git remote add origin git@github.com:qqlcx5/qqlcx5.github.io.git
-git remote set-url --add origin git@gitee.com:qqlcx5/vuepress.git
+git remote set-url --add origin git@gitee.com:qqlcx5/qqlcx5.git
 git remote set-url --add origin git@e.coding.net:qqlcx5/qqlcx5/vuepress.git
 git push -f --all
 rm -rf docs/.vuepress/dist
