4b254690e35508df69fa75505b0bed26e885ee3b
qqlcx5 <qqlcx5@163.com>
2021-02-22 17:32:04 +0800

    description

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -9,7 +9,7 @@ const plugins = [
     // 代码块复制按钮
     ['one-click-copy', {
         copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        copyMessage: '复制成功~~', // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
         showInMobile: false, // whether to display on the mobile side, default: false.
       },
