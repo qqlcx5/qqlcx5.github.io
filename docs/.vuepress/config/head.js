cd7ecf58a004ba3a526c6bf1a67c82edad709f67
qqlcx5 <qqlcx5@163.com>
2021-02-26 11:58:27 +0800

    header

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -6,7 +6,5 @@ module.exports = [
       content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'},
   ],
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
-  [
-    'script', { async: 'async', src: 'https://hm.baidu.com/hm.js?662dade0b3fe9911e80442e86935e233' },
-  ],
+  ['script', { async: 'async', src: 'https://hm.baidu.com/hm.js?662dade0b3fe9911e80442e86935e233' }],
 ];
